12e76c23d9c4298620b77d1296489642e3321f9d
envikaaa <envikapriyatna31@gmail.com>
2026-03-09 15:42:22 +0700

    menambahkan if

--- file1.cpp
+++ file1.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 int main(){
     int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout<<"nilai z sudah dikoreksi menjadi : "<<x<<<endl;
+    }
 }
