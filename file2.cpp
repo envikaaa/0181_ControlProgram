76b5aec1da066d481cd1d0f4afbc4da19c3dd25c
envikaaa <envikapriyatna31@gmail.com>
2026-03-09 15:55:46 +0700

    menambahkan string

--- file2.cpp
+++ file2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
-float RAta_rata(Float a, Float b){
+float RAta_rata(float a, float b){
     return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
 }
